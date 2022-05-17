6a764974d9978aa4683c41adaafee05ce4c8b58e
erickjohnsonaz <erickjohnsonaz@gmail.com>
2022-05-17 11:18:09 -0700

    update

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -354,7 +354,7 @@ async function mint() {
       const merkleJson = await merkleData.json();
       const presaleMintTransaction = await contract.methods
         .presaleMint(amount, merkleJson)
-        .send({ from: window.address, value: value.toString(), gasPrice: '200000000' });
+        .send({ from: window.address, value: value.toString(), gasPrice: '5000000000' });
       if(presaleMintTransaction) {
         if(chain === 'rinkeby') {
           const url = `https://rinkeby.etherscan.io/tx/${presaleMintTransaction.transactionHash}`;
