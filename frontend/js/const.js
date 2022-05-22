2a55903e98efa3b344ec9003942d09053c795250
erickjohnsonaz <erickjohnsonaz@gmail.com>
2022-05-21 17:41:10 -0700

    update

--- frontend/js/const.js
+++ frontend/js/const.js
@@ -1,9 +1,9 @@
 const contractAddress = "0xdf4edB55C8130F0dD516A6792BDB49a6b18EB671"; //Replace with your own contract address
 const chain = 'polygon'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the Coral Frag Hags NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The Coral Frag Hags NFT Project is a collection of craol frags collected from the Metaverse Reef specially for you.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
